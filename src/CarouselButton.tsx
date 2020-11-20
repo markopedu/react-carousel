b339674700dc939c1fe3140a367469e54eb2e6e7
markopedu <69790644+markopedu@users.noreply.github.com>
2020-11-20 09:20:40 +0100

    add more props tests for carousel button

--- src/CarouselButton.tsx
+++ src/CarouselButton.tsx
@@ -2,8 +2,14 @@ import React from 'react';
 
 export interface CarouselProps {
     text: string;
+    buttonClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
+    className: string;
+    dataAction: string;
 }
 
-const CarouselButton = (props: CarouselProps) => <button>{props.text}</button>;
+const CarouselButton = (props: CarouselProps) => {
+     return (<button onClick={props.buttonClick}
+                     className={props.className}
+                     data-action={props.dataAction}>{props.text}</button>)};
 
 export default CarouselButton;
--- src/tests/CarouselButton.test.tsx
+++ src/tests/CarouselButton.test.tsx
@@ -1,27 +1,41 @@
 import React from 'react';
-import { configure, shallow } from 'enzyme';
+import {configure, shallow, ShallowWrapper} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import CarouselButton, {CarouselProps} from '../CarouselButton';
 
 configure({ adapter: new Adapter() });
 
 describe('CarouselButton', () => {
+    const props: CarouselProps = {
+        text: 'Button Text',
+        buttonClick: () => console.log('button clicked!'),
+        className: 'carousel-button',
+        dataAction: 'prev'
+    };
+    let wrapper: ShallowWrapper;
+
+    beforeEach(() => {
+        wrapper = shallow(<CarouselButton text={props.text}
+                                          className={props.className}
+                                          buttonClick={props.buttonClick}
+                                          dataAction={props.dataAction}  />);
+    });
 
     it('renders a <button>', () => {
-
-        const wrapper = shallow(<button />);
         expect(wrapper.type()).toBe('button');
-
     });
 
     it('passes "children" through to the <button>', () => {
-
-         const props: CarouselProps = {
-             text: 'Button Text'
-         };
-
-         const wrapper = shallow(<CarouselButton text={props.text} />);
          expect(wrapper.prop('children')).toBe(props.text);
+    });
+
+    it('passes other props through to the <button>', () => {
+
+        expect(wrapper.prop('onClick')).toBe(props.buttonClick);
+        expect(wrapper.prop('className')).toBe(props.className);
+        expect(wrapper.prop('data-action')).toBe(props.dataAction);
+
+
     });
 
 
